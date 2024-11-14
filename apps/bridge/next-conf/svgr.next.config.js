e5d602fd00568a278c73682f5ecb14470711d2f6
venuswhispers <venuswhisperer412@gmail.com>
2024-11-14 13:18:28 -0500

    before: update next config

--- apps/bridge/next-conf/svgr.next.config.js
+++ apps/bridge/next-conf/svgr.next.config.js
@@ -1,36 +1,29 @@
-const watchExternalFilesPlugin = require('webpack-watch-files-plugin').default
+// https://react-svgr.com/docs/next
 
-const disable = false
-
-const localModules = [
-  //'../../../../hanzoai/react-sdk/packages/ui',
-  //'../../../../hanzoai/react-sdk/packages/commerce'
-]
-
-const suffixes = [
-  '/**/*.ts',
-  '/**/*.tsx',
-]
-
-const flatArray = localModules.map((p) => {
-  return suffixes.map((s) => (p + s))
-}).flat()
-
-
-module.exports = ( config ) => {
-
-  if (disable) {
-    return config
-  }
-
-  config.watchOptions = {
-    poll: 1000,
-    aggregateTimeout: 300,
-  }
-  config.plugins.push(
-    new watchExternalFilesPlugin({
-      files: flatArray,
-    })
+module.exports = (config) => {
+  // Grab the existing rule that handles SVG imports
+  const fileLoaderRule = config.module.rules.find((rule) =>
+    rule.test?.test?.('.svg'),
   )
+
+  config.module.rules.push(
+    // Reapply the existing rule, but only for svg imports ending in ?url
+    {
+      ...fileLoaderRule,
+      test: /\.svg$/i,
+      resourceQuery: /url/, // *.svg?url
+    },
+    // Convert all other *.svg imports to React components
+    {
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+      use: ['@svgr/webpack'],
+    },
+  )
+
+  // Modify the file loader rule to ignore *.svg, since we have it handled now.
+  fileLoaderRule.exclude = /\.svg$/i
+
   return config
 }
